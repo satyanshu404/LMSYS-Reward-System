21509deb608c68522eb9f56eb8276971c8af9233
Satyanshu kumar <67653823+satyanshu404@users.noreply.github.com>
2024-07-02 23:39:45 +0530

    [major] now passing lablel as well

--- constants.py
+++ constants.py
@@ -4,5 +4,8 @@ from dataclasses import dataclass, field
 class ResponseGeneratorConstants:
     # FILE_PATH: str = 'data/small-train-gpt-4o-2024-05-13_1.csv'
     FILE_PATH:str = 'data/small-train-nemotron-4-340b_1.csv'
-    START: int = 17
-    NUM_ROWS: int = 30
+    # FILE_PATH:str = 'data/small-train-claude-3-opus-20240229_1.csv'
+    # START: int = 17
+    START: int = 0
+    NUM_ROWS: int = 200
+    NUM_OF_ITERATIONS: int = 10
