5f817a520c95e8a09668d4e9c68d6e8662d833a1
Satyanshu kumar <67653823+satyanshu404@users.noreply.github.com>
2024-07-30 01:03:12 +0530

    [minor] added constants for Creating dataset and with some changes in response genreator const

--- constants.py
+++ constants.py
@@ -2,17 +2,22 @@ from dataclasses import dataclass, field
 
 @dataclass
 class ResponseGeneratorConstants:
-    # FILE_PATH:str = 'data/small-train-nemotron-4-340b_1.csv'
-    FILE_PATH:str = 'data/small-train-5000-gemma-2-27b-it.csv'
-    # FILE_PATH:str = 'data/qwen_test_df_output.csv'
-    # FILE_PATH:str = 'data/small-train-qwen2-72b-instruct.csv'
-    # START: int = 17
+    FILE_PATH:str = 'data/train-sliced-5000-20000-llama-3.1.csv'
     START: int = 0
-    NUM_ROWS: int = 658
+    NUM_ROWS: int = 18598
     NUM_OF_ITERATIONS: int = 10
     ERROR_LIST: list = field(default_factory=lambda: ['API REQUEST ERROR', 
                                                       'NETWORK ERROR DUE TO HIGH TRAFFIC'])
     
 @dataclass
 class ThreadConstants:
-    WAIT_DURATION: int = 298
+    WAIT_DURATION: int = 300
+
+
+@dataclass
+class CreateDataConstants:
+    DEFAULT_FILE_PATH:str = 'data/train.csv'
+    SLICE: str = '5000:20000'
+    MAX_TEXT_SIZE: int = 4500
+    DEFAULT_VALUE: str = 'No Value'
+    OUTPUT_FILE_PATH: str = 'data/train-sliced-' + '-'.join(SLICE.split(':')) + '.csv'
